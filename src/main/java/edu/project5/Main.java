179c9086a732629e2024c8c8aca41c529fa4c394
TheMLord <mikhail.berezin.03@mail.ru>
2023-12-17 12:46:21 +0500

    project5 Berezin

---
+++ src/main/java/edu/project5/Main.java
@@ -0,0 +1,41 @@
+package edu.project5;
+
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+/**
+ * Main class.
+ */
+public final class Main {
+    private Main() {
+    }
+
+    /**
+     * Method main.
+     * The configuration for the benchmark is being performed and it is being launched
+     */
+    @SuppressWarnings("MagicNumber")
+    public static void main(String[] args)
+        throws RunnerException {
+        Options options = new OptionsBuilder()
+            .include(ReflectionBenchmark.class.getSimpleName())
+            .shouldFailOnError(true)
+            .shouldDoGC(true)
+            .mode(Mode.AverageTime)
+            .timeUnit(TimeUnit.NANOSECONDS)
+            .forks(1)
+            .warmupForks(1)
+            .warmupIterations(1)
+            .warmupTime(TimeValue.seconds(5))
+            .measurementIterations(1)
+            .measurementTime(TimeValue.seconds(5))
+            .build();
+
+        new Runner(options).run();
+    }
+}
---
+++ src/main/java/edu/project5/Student.java
@@ -0,0 +1,10 @@
+package edu.project5;
+
+/**
+ * Student models for performing performance tests of ways to access the name() method
+ *
+ * @param name    student name.
+ * @param surname student surname.
+ */
+public record Student(String name, String surname) {
+}
