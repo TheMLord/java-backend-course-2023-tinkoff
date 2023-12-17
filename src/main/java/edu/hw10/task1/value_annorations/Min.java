a09afc00d160e86ca3df618501a6ab801d0c9c36
TheMLord <mikhail.berezin.03@mail.ru>
2023-12-17 16:37:35 +0500

    add Min Max NotNull annotations javadoc

--- src/main/java/edu/hw10/task1/value_annorations/Min.java
+++ src/main/java/edu/hw10/task1/value_annorations/Min.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marking annotation for specifying the minimum boundary of the values of an argument of type int,
+ * double and their object wrappers
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface Min {
--- src/main/java/edu/hw10/task1/value_annorations/NotNull.java
+++ src/main/java/edu/hw10/task1/value_annorations/NotNull.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marking annotation indicating that the object cannot be null.
+ * If the object does not have such an annotation, then it is guaranteed to be null
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface NotNull {
